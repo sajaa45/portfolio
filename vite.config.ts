1eaad1bc8bd7f8e64d6ae7a5702e0b30799cb9b4
sajaa45 <moussa.saja@gmail.com>
2025-02-03 23:56:41 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/portfolio/",
   plugins: [react()],
-})
+});
+
